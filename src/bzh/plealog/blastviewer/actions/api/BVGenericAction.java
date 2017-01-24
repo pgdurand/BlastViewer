686cc9516dad39752ec76de326da90e5dd77197f
pgdurand <patrick@plealog.com>
2017-01-24 12:02:21 +0100

    add MSA and PhyloTree viewers

--- src/bzh/plealog/blastviewer/actions/api/BVGenericAction.java
+++ src/bzh/plealog/blastviewer/actions/api/BVGenericAction.java
@@ -19,7 +19,6 @@ package bzh.plealog.blastviewer.actions.api;
 import java.awt.event.ActionEvent;
 
 import javax.swing.AbstractAction;
-import javax.swing.Icon;
 import javax.swing.SwingUtilities;
 
 import bzh.plealog.bioinfo.ui.blast.hittable.BlastHitTable;
