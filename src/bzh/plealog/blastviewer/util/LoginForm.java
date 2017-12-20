97dec0f1739dfb5e4ccd21d6728203662541e696
Patrick DURAND <patrick@plealog.com>
2017-12-20 17:05:54 +0100

    add LoginForm panel

---
+++ src/bzh/plealog/blastviewer/util/LoginForm.java
@@ -0,0 +1,145 @@
+/* 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ */
+package bzh.plealog.blastviewer.util;
+
+import java.awt.BorderLayout;
+import java.awt.Component;
+
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
+import com.jgoodies.forms.builder.DefaultFormBuilder;
+import com.jgoodies.forms.layout.FormLayout;
+
+/**
+ * A basic form to ask a user for credentials.
+ * 
+ * @author Patrick G. Durand
+ */
+public class LoginForm {
+
+  private JPanel         _loginForm;
+  private JTextField     _userNameField;
+  private JPasswordField _passwordField;
+
+  private static final String FORM_LBL  = "Set crendentials";
+  private static final String UNAME_LBL = "User name:";
+  private static final String PSWD_LBL  = "Password:";
+  
+  /**
+   * Constructor.
+   */
+  public LoginForm() {
+    initializeLoginForm();
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param username user name to set in the userName field
+   */
+  public LoginForm(String username) {
+    this();
+    setUserName(username);
+  }
+
+  /**
+   * Constructor.
+   * 
+   * @param username user name to set in the userName field
+   * @param password password to set in the password field
+   */
+  public LoginForm(String username, String password) {
+    this(username);
+    setPassword(password);
+  }
+
+  /**
+   * Display the login form as a model dialogue box.
+   * 
+   * @param parent parent component of this dialogue box
+   * 
+   * @return true if the user closes the dialogue box using OK button,
+   * false otherwise.
+   * */
+  public boolean displayLoginForm(Component parent) {
+    int retVal = JOptionPane.showConfirmDialog(
+        parent, 
+        _loginForm, 
+        FORM_LBL, 
+        JOptionPane.OK_CANCEL_OPTION, 
+        JOptionPane.QUESTION_MESSAGE);
+    return (retVal==JOptionPane.OK_OPTION);
+  }
+
+  /**
+   * Create a login form.
+   */
+  private void initializeLoginForm() {
+    DefaultFormBuilder builder;
+    FormLayout layout;
+    
+    _loginForm = new JPanel(new BorderLayout(5, 5));
+    _userNameField = new JTextField();
+    _passwordField = new JPasswordField();
+    
+    layout = new FormLayout("40dlu, 1dlu, 100dlu", "");
+    builder = new DefaultFormBuilder(layout);
+    builder.setDefaultDialogBorder();
+    
+    builder.append(UNAME_LBL, _userNameField);
+    builder.nextLine();
+    builder.append(PSWD_LBL, _passwordField);
+    _loginForm.add(builder.getContainer(), BorderLayout.CENTER);
+    
+  }
+
+  /**
+   * Get the user name.
+   * @return a user name
+   */
+  public String getUserName() {
+    return _userNameField.getText();
+  }
+
+  /**
+   * Set the user name.
+   * 
+   * @param username a user name
+   * */
+  public void setUserName(String username) {
+    _userNameField.setText(username);
+  }
+
+  /**
+   * Get the password.
+   * @return a password
+   */
+  public char[] getPasword() {
+    return _passwordField.getPassword();
+  }
+
+  /**
+   * Set the password.
+   * 
+   * @param password a password
+   * */
+  public void setPassword(String password) {
+    _passwordField.setText(password);
+  }
+}
---
+++ src/test/LoginFormTest.java
@@ -0,0 +1,57 @@
+/* 
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  You may obtain a copy of the License at
+ *
+ *     https://www.gnu.org/licenses/agpl-3.0.txt
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Affero General Public License for more details.
+ */
+package test;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import bzh.plealog.blastviewer.util.LoginForm;
+
+/**
+ * A sample application to test the LoginForm class.
+ * 
+ * @author Patrick G. Durand
+ */
+public class LoginFormTest {
+  public static void main(String[] args) {
+    Runnable r = new Runnable() {
+
+      @Override
+      public void run() {
+        JLabel credentialsLbl = new JLabel("<html><body style='width: 300px; height: 175px;></body></html>", SwingConstants.CENTER);
+        JFrame appFrame = new JFrame("LoginFormTest");
+        appFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        appFrame.setLocationByPlatform(true);
+        appFrame.add(credentialsLbl);
+        appFrame.pack();
+        appFrame.setMinimumSize(appFrame.getSize());
+        appFrame.setVisible(true);
+
+        LoginForm lif = new LoginForm();
+        lif.setUserName(System.getProperty("user.name"));
+        if (lif.displayLoginForm(appFrame)) {
+          credentialsLbl.setText(String.format("Welcome %s (%s)", lif.getUserName(), new String(lif.getPasword())));
+        } else {
+          credentialsLbl.setText("No credentials set!");
+        }
+      }
+    };
+    SwingUtilities.invokeLater(r);
+  }
+
+}
